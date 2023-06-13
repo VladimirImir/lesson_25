805920038b2e9a079aae9a1d94202a0a3438b5a1
VladimirImir <mimimvvv@gmail.com>
2023-06-13 03:38:38 +0300

    25.2 - Интерфейс Runnable.

---
+++ src/com/dev/lesson25/SimpleRunnable.java
@@ -0,0 +1,9 @@
+package com.dev.lesson25;
+
+public class SimpleRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Hello from Runnable: " + Thread.currentThread().getName());
+    }
+}
