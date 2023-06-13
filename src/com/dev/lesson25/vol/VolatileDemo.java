d58a848d59561297df3133f0c019e3793cbbda6c
VladimirImir <mimimvvv@gmail.com>
2023-06-13 21:59:04 +0300

    25.7 - Ключевое слово volatile.

---
+++ src/com/dev/lesson25/vol/VolatileDemo.java
@@ -0,0 +1,32 @@
+package com.dev.lesson25.vol;
+
+/**
+ * [RAM    flag=false]
+ *
+ * 1 -----[flag=false]
+ *
+ * 2 -----[flag=true]
+ *
+ */
+
+public class VolatileDemo {
+
+    private static volatile boolean flag = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            while (!flag) {
+                System.out.println("still false");
+            }
+        });
+        thread1.start();
+
+        Thread.sleep(5L);
+
+        Thread thread2 = new Thread(() -> {
+            flag = true;
+            System.out.println("flag is set");
+        });
+        thread2.start();
+    }
+}
