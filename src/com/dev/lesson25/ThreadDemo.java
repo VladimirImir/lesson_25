a77447fb1862d75f9c455bbb020dbb449ee47af2
VladimirImir <mimimvvv@gmail.com>
2023-06-13 03:49:30 +0300

    25.3 - Java. Состояния потоков.

--- src/com/dev/lesson25/ThreadDemo.java
+++ src/com/dev/lesson25/ThreadDemo.java
@@ -4,14 +4,21 @@ public class ThreadDemo {
 
     public static void main(String[] args) {
         var simpleThread = new SimpleThread();
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
         var runnableThread = new Thread(new SimpleRunnable(), "My Name");
         var lambdaThread = new Thread(() -> System.out.println("Hello from lambda: " + Thread.currentThread().getName()));
 
         simpleThread.start();
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
+
         runnableThread.start();
         lambdaThread.start();
         try {
-            simpleThread.join();
+            /** Если simpleThread не успеет выполниться за 100 миллисекунд -
+             *  потом main все равно продолжить выполнение дальше. */
+            simpleThread.join(100L);
+            System.out.println(simpleThread.getName() + ": " + simpleThread.getState()); 
+
             runnableThread.join();
             lambdaThread.join();
         } catch (InterruptedException e) {
