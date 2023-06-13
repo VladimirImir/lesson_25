da1fcfacefd580f1a82461a4f18583ac6effb3f9
VladimirImir <mimimvvv@gmail.com>
2023-06-13 19:28:13 +0300

    25.4 - Атомарность операций.

---
+++ src/com/dev/lesson25/counter/CounterThread.java
@@ -0,0 +1,24 @@
+package com.dev.lesson25.counter;
+
+/**
+ * 2. Создать класс CounterThread c одним полем и конструктором для инициализации:
+ * private Counter counter;
+ * <p>
+ * В методе run этого класса в цикле 100 раз вызвать counter.increment()
+ */
+
+public class CounterThread extends Thread {
+
+    private final Counter counter;
+
+    public CounterThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            counter.increment();
+        }
+    }
+}
